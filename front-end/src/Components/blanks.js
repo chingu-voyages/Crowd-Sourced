84a5b84b904589400d5b016a23dc55496273ebf9
josue rojas <josuerojas.rojas@gmail.com>
2018-12-03 21:30:03 -0500

    init code / basic setup

---
+++ front-end/src/Components/blanks.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Blank extends Component {
+  render(){
+    return(
+      <div className='test'>Test component</div>
+    )
+  }
+}
