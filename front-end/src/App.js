6f195da8d0992ffac447cb3f307b4e3693bfbba0
josue rojas <josuerojas.rojas@gmail.com>
2018-12-06 18:39:27 -0500

    basic desktop navbar design

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import './Styles/App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import HomePage from './Components/HomePage';
 import AboutPage from './Components/AboutPage';
 import ListingsPage from './Components/ListingsPage';
 import LoginPage from './Components/LoginPage';
 
+// Component that uses withRouter to get current location and send it to the parent class using a callback
+// https://reacttraining.com/react-router/web/api/withRouter
+const LocationListenerRouter = withRouter(
+  class LocationListener extends Component {
+    static propTypes = {
+      match: PropTypes.object.isRequired,
+      location: PropTypes.object.isRequired,
+      history: PropTypes.object.isRequired
+    };
+
+    componentDidUpdate(prevProps){
+      if (this.props.location !== prevProps.location) {
+        this.props.locationcallback(this.props.location);
+      }
+    }
+
+    render(){
+      return null;
+    }
+  });
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      location: window.location
+    }
+    this.updateLocation = this.updateLocation.bind(this);
+  }
+
   componentDidMount() {
     fetch('/test')
       .then((res) => { return res.json()} )
@@ -16,11 +45,17 @@ class App extends Component {
         console.log(data);
       });
   }
+
+  updateLocation(location){
+    this.setState({location: location});
+  }
+
   render() {
     return (
       <Router>
         <div className='App'>
-          <NavBar/>
+          <LocationListenerRouter locationcallback={this.updateLocation}/>
+          <NavBar currentPage={this.state.location}/>
           <Route path='/' exact component={HomePage} />
           <Route path='/about' component={AboutPage} />
           <Route path='/listings' component={ListingsPage} />
--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -7,6 +7,7 @@ export default class NavBar extends Component {
     super(props);
     this.state = {
       isMenuActive: false,
+      linksHover: false,
     }
     this.toggleMenu = this.toggleMenu.bind(this);
   }
@@ -14,6 +15,7 @@ export default class NavBar extends Component {
     this.setState({isMenuActive: !this.state.isMenuActive});
   }
   render(){
+    const currentlink = this.props.currentPage.pathname;
     return(
       <nav className={`nav-bar ${this.state.isMenuActive ? 'active' :  ''}`}>
         <div className='menu-icon-wrapper'>
@@ -25,24 +27,27 @@ export default class NavBar extends Component {
           </div>
         </div>
         <div className='links-wrapper'>
-          <ul className='links'>
+          <ul
+            className={`links ${this.state.linksHover ? 'hover' : ''}`}
+            onMouseEnter={()=>this.setState({linksHover: true})}
+            onMouseLeave={()=>this.setState({linksHover: false})}>
             <li
-            className='link'
-            onClick={this.toggleMenu}>
+              className={`link ${currentlink === '/' ? 'current' : ''}`}
+              onClick={this.toggleMenu}>
               <Link to='/'>Home</Link>
             </li>
             <li
-              className='link'
+              className={`link ${currentlink === '/about' ? 'current' : ''}`}
               onClick={this.toggleMenu}>
               <Link to='/about'>About</Link>
             </li>
             <li
-              className='link'
+              className={`link ${currentlink === '/listings' ? 'current' : ''}`}
               onClick={this.toggleMenu}>
               <Link to='/listings'>Listings</Link>
             </li>
             <li
-              className='link'
+              className={`link ${currentlink === '/login' ? 'current' : ''}`}
               onClick={this.toggleMenu}>
               <Link to='/login'>Log In</Link>
             </li>
