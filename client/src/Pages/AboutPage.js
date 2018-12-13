6ed5db4fb975a7234c9c5e617e6d6f4a14082701
Viren Bhagat <virenb@gmail.com>
2018-12-12 22:56:52 -0500

    Started about page, will have to add more content

--- client/src/Pages/AboutPage.js
+++ client/src/Pages/AboutPage.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import '../Styles/AboutPage.css';
 
 export default class AboutPage extends Component {
-  render(){
-    return(
-      <div className='page page-about'>About</div>
+  render() {
+    return (
+      <div className='page page-about'>
+        <h1 className='title'>About Crowdsourced</h1>
+        <div class='about-main'>
+          Crowdsourced is a project created late 2018. In short, think Craigslist + GoFundMe.
+          Will update this more.
+        </div>
+      </div>
     )
   }
 }
