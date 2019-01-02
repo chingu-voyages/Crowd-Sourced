779b64b4110f9775e1d5cad3c1e6a4cb853cbe91
Viren Bhagat <virenb@gmail.com>
2019-01-02 16:39:22 -0500

    Small fixes on about page, responsive for title, class --> className

--- client/src/Pages/AboutPage.js
+++ client/src/Pages/AboutPage.js
@@ -6,7 +6,7 @@ export default class AboutPage extends Component {
     return (
       <div className='page page-about'>
         <h1 className='title'>About Crowdsourced</h1>
-        <div class='about-main'>
+        <div className='about-main'>
           Crowdsourced is a project created late 2018. In short, think Craigslist + GoFundMe.
           Will update this more.
         </div>
