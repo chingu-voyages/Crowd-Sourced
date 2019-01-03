031fcade22542b8f1a8cb1bd2418a7a2e57045a2
Viren Bhagat <virenb@gmail.com>
2019-01-03 18:29:04 -0500

    Added apollo, connected it to server, made initial items query

--- client/src/Pages/ListingsOffersPage.js
+++ client/src/Pages/ListingsOffersPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import { SiteLinkCard } from '../Components/Card';
 import '../Styles/ListingsOffersPage.css';
 
@@ -6,12 +8,28 @@ export default class ListingsOffersPage extends Component {
   render(){
     return(
       <div className='page page-offers'>
-        <SiteLinkCard link='/'>test</SiteLinkCard>
-        <SiteLinkCard link='/'>test</SiteLinkCard>
-        <SiteLinkCard link='/'>test</SiteLinkCard>
-        <SiteLinkCard link='/'>test</SiteLinkCard>
-        <SiteLinkCard link='/'>test</SiteLinkCard>
-        <SiteLinkCard link='/'>test</SiteLinkCard>
+        <Query
+          query={gql`
+            {
+              items {
+                name
+                id
+                category
+              }
+            }
+          `}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+            return data.items.map(item => (
+              <SiteLinkCard link='/' key={item.id}>
+                <h3>{item.name}</h3>
+                <p>{item.category}</p>
+              </SiteLinkCard>
+            ));
+          }}
+        </Query>             
       </div>
     )
   }
