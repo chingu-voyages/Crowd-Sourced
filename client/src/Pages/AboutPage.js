b9b9f860fd38f7efb2d4e29c1b026beb704f425f
josue rojas <josuerojas.rojas@gmail.com>
2019-01-04 15:12:02 -0500

    update title

--- client/src/Pages/AboutPage.js
+++ client/src/Pages/AboutPage.js
@@ -5,9 +5,9 @@ export default class AboutPage extends Component {
   render() {
     return (
       <div className='page page-about'>
-        <h1 className='title'>About Crowdsourced</h1>
+        <h1 className='title'>About Crowd Sourced</h1>
         <div className='about-main'>
-          Crowdsourced is a project created late 2018. In short, think Craigslist + GoFundMe.
+          Crowd Sourced is a project created late 2018. In short, think Craigslist + GoFundMe.
           Will update this more.
         </div>
       </div>
