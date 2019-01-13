01424f325339ceb67761e6238dff7cb0f0db6cc2
Viren Bhagat <virenb@gmail.com>
2019-01-13 17:18:38 -0500

    Added dotenv package to hide mlab login

--- server/index.js
+++ server/index.js
@@ -4,13 +4,16 @@ const path = require('path');
 const parser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const env = require('dotenv').config();
 const app = express();
 
 app.use(cors());
 
-mongoose.connect('mongodb://chingu:test123@ds127944.mlab.com:27944/bears-13', { useNewUrlParser: true });
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds127944.mlab.com:27944/bears-13`, {
+	useNewUrlParser: true
+});
 mongoose.connection.once('open', () => {
-  console.log('connected to db');
+	console.log('connected to db');
 });
 
 const schema = require('./schema/schema');
@@ -19,15 +22,18 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../build/', 'index.html'));
+	res.sendFile(path.resolve(__dirname, '../build/', 'index.html'));
 });
 
-app.listen(PORT, function () {
-  console.error(`Listening on port: ${PORT}`);
+app.listen(PORT, function() {
+	console.error(`Listening on port: ${PORT}`);
 });
