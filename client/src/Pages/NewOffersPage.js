b6355061cae9126ad6e41b1b92e83b6be12fc20c
josue rojas <josuerojas.rojas@gmail.com>
2019-01-18 19:17:54 -0500

    added loading animation

--- client/src/Pages/NewOffersPage.js
+++ client/src/Pages/NewOffersPage.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 import SingleCard from '../Components/SingleCard';
 import { FormButton } from '../Components/Buttons';
+import Loading from '../Components/Loading';
 import TextInput from '../Components/TextInput';
 import '../Styles/NewOffersPage.css';
 import {hasInput, emailCheck, zipCheck } from '../Components/InputChecks';
@@ -132,56 +133,59 @@ export default class NewOffersPage extends Component {
 						}}
 					>
 						{(add_item, { loading, error })=>(
-							<form
-								onSubmit={e=>{
-									e.preventDefault();
-									this.submitForm(add_item)
-								}}>
-								<label>Name</label>
-								<TextInput
-									value={this.state['name'].val}
-									onChange={(e) => this.onInputChange(e, 'name')}
-									hasError={this.state['name'].hasError}
-									placeholder="Item name"
-									required
-								/>
-								<label>Category</label>
-								<TextInput
-									value={this.state['category'].val}
-									onChange={(e) => this.onInputChange(e, 'category')}
-									hasError={this.state['category'].hasError}
-									placeholder="Item category"
-									required
-								/>
-								<label>Description</label>
-								<TextInput
-									value={this.state['description'].val}
-									onChange={(e) => this.onInputChange(e, 'description')}
-									hasError={this.state['description'].hasError}
-									placeholder="Item description"
-									required
-								/>
-								<label>E-Mail</label>
-								<TextInput
-									value={this.state['email'].val}
-									onChange={(e) => this.onInputChange(e, 'email')}
-									type="email"
-									hasError={this.state['email'].hasError}
-									placeholder="Your E-Mail"
-									required
-								/>
-								<label>Location</label>
-								<TextInput
-									value={this.state['location'].val}
-									onChange={(e) => this.onInputChange(e, 'location')}
-									hasError={this.state['location'].hasError}
-									placeholder="Enter 5 digit zip code"
-									required
-								/>
-								<div className="button-wrapper">
-									<FormButton text="Submit" />
-								</div>
-							</form>
+							<div className='form-wrapper'>
+								<form
+									onSubmit={e=>{
+										e.preventDefault();
+										this.submitForm(add_item)
+									}}>
+									<label>Name</label>
+									<TextInput
+										value={this.state['name'].val}
+										onChange={(e) => this.onInputChange(e, 'name')}
+										hasError={this.state['name'].hasError}
+										placeholder="Item name"
+										required
+									/>
+									<label>Category</label>
+									<TextInput
+										value={this.state['category'].val}
+										onChange={(e) => this.onInputChange(e, 'category')}
+										hasError={this.state['category'].hasError}
+										placeholder="Item category"
+										required
+									/>
+									<label>Description</label>
+									<TextInput
+										value={this.state['description'].val}
+										onChange={(e) => this.onInputChange(e, 'description')}
+										hasError={this.state['description'].hasError}
+										placeholder="Item description"
+										required
+									/>
+									<label>E-Mail</label>
+									<TextInput
+										value={this.state['email'].val}
+										onChange={(e) => this.onInputChange(e, 'email')}
+										type="email"
+										hasError={this.state['email'].hasError}
+										placeholder="Your E-Mail"
+										required
+									/>
+									<label>Location</label>
+									<TextInput
+										value={this.state['location'].val}
+										onChange={(e) => this.onInputChange(e, 'location')}
+										hasError={this.state['location'].hasError}
+										placeholder="Enter 5 digit zip code"
+										required
+									/>
+									<div className="button-wrapper">
+										<FormButton text="Submit" />
+									</div>
+								</form>
+								{loading && <div className='loading-cover'><Loading/></div>}
+							</div>
 						)}
 					</Mutation>
 				</SingleCard>
